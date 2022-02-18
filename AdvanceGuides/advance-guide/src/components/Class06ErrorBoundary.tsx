d8d3130a2eeb861dca8ac5000d4ec27bb4a8e411
Gibmyx <gibmyxjobs@gmail.com>
2022-02-18 08:55:33 -0400

    feat: manejo de errores.

---
+++ AdvanceGuides/advance-guide/src/components/Class06ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import {Component, ErrorInfo} from "react";
+
+class Class06ErrorBoundary extends Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = { error: null, errorInfo: null };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+    }
+
+    render() {
+        if (this.state.errorInfo) {
+            return (
+                <div>
+                    <h2>Algo salió mal.</h2>
+                    <details style={{ whiteSpace: 'pre-wrap' }}>
+                        {this.state.error && this.state.error.toString()}
+                        <br />
+                        {this.state.errorInfo.componentStack}
+                    </details>
+                </div>
+            );
+        }
+        // Normally, just render children
+        return this.props.children;
+    }
+}
+
+export {Class06ErrorBoundary}
